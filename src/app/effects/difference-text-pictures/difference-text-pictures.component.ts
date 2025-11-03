75a0ff8d54cbb198fa5cc1a6cb7fc89ac5f3a018
Antonio Yepes <aeyepes1999@gmail.com>
2025-11-03 11:25:50 +0100

    Finished difference effect

--- src/app/effects/difference-text-pictures/difference-text-pictures.component.ts
+++ src/app/effects/difference-text-pictures/difference-text-pictures.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
@@ -13,18 +13,18 @@ import { CommonModule } from '@angular/common';
 })
 export class DifferenceTextPicturesComponent implements AfterViewInit{
 
-  images: {src: string, width: string, transformX: string}[] = [
-    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", transformX: "translateX(-100%)"}, 
-    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", transformX: "translateX(100%)"}, 
-    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", transformX: "translateX(-100%)"}, 
-    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", transformX: "translateX(100%)"}, 
-    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", transformX: "translateX(-100%)"}, 
-    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", transformX: "translateX(100%)"}, 
-    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", transformX: "translateX(-100%)"}, 
-    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", transformX: "translateX(100%)"}, 
-    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", transformX: "translateX(-100%)"}, 
-    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", transformX: "translateX(100%)"}, 
-    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", transformX: "translateX(-100%)"}, 
+  images: {src: string, width: string, pos: string}[] = [
+    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", pos: 'left'}, 
+    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "45%", pos: 'left'}, 
+    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "60%", pos: 'right'}, 
+    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "70%", pos: 'left'}, 
+    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "10%", pos: 'right'}, 
+    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "20%", pos: 'left'}, 
+    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "30%", pos: 'right'}, 
+    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "60%", pos: 'left'}, 
+    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "20%", pos: 'left'}, 
+    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "80%", pos: 'right'}, 
+    {src: "https://urbanjurgensen.com/cdn/407a2850f1b873253bda920257ba286620ac4ec1-8193x10241.jpg?fp-x=0.5&fp-y=0.5&w=960&h=1280&q=75&fit=crop&auto=format", width: "10%", pos: 'right'}, 
   ];
 
   ngAfterViewInit(){
@@ -33,9 +33,9 @@ export class DifferenceTextPicturesComponent implements AfterViewInit{
 
   pinText(){
     ScrollTrigger.create({
-      trigger: 'h1',
-      start: 'top .container2',
-      endTrigger: ".container2",
+      trigger: '#titleDifference',
+      start: 'top .container',
+      endTrigger: ".container",
       end: 'bottom bottom',
       pin: true,
       pinSpacing: false,
